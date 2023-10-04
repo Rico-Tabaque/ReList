32e186ccc8e5f0b72c25424a8444c966609cd8d6
RicoTabaque <rico.tabaque@macrin.ph>
2023-10-04 13:48:52 +0800

    Added Connection string

---
+++ WebApplication/DatabaseContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<User>? Users { get; set; }
+    private readonly IConfiguration Configuration;
+    public DatabaseContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseOracle(Configuration.GetConnectionString("OracleDatabase"));
+    }
+}
