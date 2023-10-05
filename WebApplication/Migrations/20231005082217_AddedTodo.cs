9913de1a4e150d9db2cbe5c57e3e8935a5173fb6
Exequiel Arroyo <zekielarroyo@gmail.com>
2023-10-05 16:24:52 +0800

    Added User Model & Migrations

---
+++ WebApplication/Migrations/20231005082217_AddedTodo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
