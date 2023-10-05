0a90dc7fce0469cd3ed6724869142232a0fd196d
RicoTabaque <rico.tabaque@macrin.ph>
2023-10-05 17:02:00 +0800

    Added Relationship

--- WebApplication/Models/Todo.cs
+++ WebApplication/Models/Todo.cs
@@ -4,4 +4,8 @@ public class Todo
     public string Title { get; set; }
     public string Description { get; set; }
     public bool IsActive { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+
 }
