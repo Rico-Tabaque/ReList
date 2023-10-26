9fa295a8c00baf7dd7d034dc7eaf3e51e054aa7c
Exequiel Arroyo <zekielarroyo@gmail.com>
2023-10-26 12:45:58 +0800

    Updated Navigation & Added  Registraition

--- BlazorApp/tailwind.config.js
+++ BlazorApp/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{razor,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./**/*.{razor,html}"],
+    theme: {
+        extend: {
+            "default": ['Consolas', 'system-ui']
+        },
+    },
+    plugins: [],
 }
 
